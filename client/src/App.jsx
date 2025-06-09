4a3d86fc6fd1a10a744b20089acc1bbc4856a7b3
Prathameshkhodke <prathameshkhodke5gmail.com>
2025-06-09 23:05:28 +0530

    registration form completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [name, setName] = useState("")
+  const[email, setEmail] = useState("")
+  const[phone, setPhone] = useState("")
+  const[password, setPassword] = useState("")
+  console.log('myName',name);
+  console.log('mhyEmail',email);
+  console.log('myPhone',phone);
+  console.log('myPassword',password);
+  
   return (
     <div>
-      Prathamesh
+      <h1>Registration form</h1>
+      <input type="text" onChange={(e)=>setName(e.target.value)} placeholder='Enter your name' /><br /><br />
+      <input type="email" onChange={(e)=>setEmail(e.target.value)} placeholder='Enter your email' /><br /><br />
+      <input type="number" onChange={(e)=>setPhone(e.target.value)} placeholder='Enter your number' /><br /><br />
+      <input type="password" onChange={(e)=>setPassword(e.target.value)} placeholder='Enter your password' /><br /><br />
     </div>
   )
 }
